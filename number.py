52978b26895922d9201f955dbf0b646c5e7e9382
Plwangg <plwanggg@gmail.com>
2019-08-23 00:06:25 +0800

    version 1

--- number.py
+++ number.py
@@ -1,13 +1,17 @@
 import random
-answer = random.randint(1,100)
+end = input('upper bound is:')
+end = int(end)
+answer = random.randint(1,end)
 answer = int(answer)
+i = 0
 while True:
+	i = i + 1
 	number = input('pls guess what computer think?')
 	number = int(number)
 	if number == answer:
-		print('you win!')
+		print('you win in', i,'times')
 		break
 	elif answer > number:
-		print('bigger')
+		print('bigger,this is', i,'time you guess')
 	else:
-		print('smaller')
+		print('smaller,this is', i,'time you guess')
